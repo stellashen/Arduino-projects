237989c1a870d14bf8edf8bb9655b45c64d81d84
Yangchen Shen <shyangch@gmail.com>
2016-08-29 20:50:45 -0500

    add millis()

--- studios/studio0/heartbeat/heartbeat.ino
+++ studios/studio0/heartbeat/heartbeat.ino
@@ -8,5 +8,9 @@ void loop() {
 	i = i + 1;
 	Serial.print(i);
 	Serial.println("sec have elapsed");
+	Serial.print("Time: ");
+	time = millis();
+	//prints time since program started
+	Serial.println(time);
 	delay(1000);
 }

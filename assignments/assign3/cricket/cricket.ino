247ba265b0e8d10311f2d0b8c05bec1986793151
Yangchen Shen <shyangch@gmail.com>
2016-09-13 17:59:40 -0500

    adding files

---
+++ assignments/assign3/cricket/cricket.ino
@@ -0,0 +1,25 @@
+/* cricket
+ *  
+ *  CSE 132 - Assignment 3
+ *  
+ *  Fill this out so we know whose assignment this is.
+ *  
+ *  Name:
+ *  WUSTL Key:
+ *  
+ *  and if two are partnered together
+ *  
+ *  Name:
+ *  WUSTL Key:
+ */
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ studios/studio3/stoplight/stoplight.ino
@@ -0,0 +1,29 @@
+/* stoplight
+ *  
+ *  CSE 132 - Studio 3
+ *  
+ *  Fill this out so we know whose studio this is.
+ *  
+ *  Name:
+ *  WUSTL Key:
+ *  
+ *  Name:
+ *  WUSTL Key:
+ *  
+ *  Name:
+ *  WUSTL Key:
+ *  
+ *  Name:
+ *  WUSTL Key:
+ */
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

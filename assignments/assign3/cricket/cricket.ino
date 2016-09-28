5d355d83ab71533979999ca2d9f8d7fff4f1c720
Yangchen Shen <shyangch@gmail.com>
2016-09-28 14:31:35 -0500

    finish assign3

--- assignments/assign3/cricket/cricket.ino
+++ assignments/assign3/cricket/cricket.ino
@@ -17,6 +17,7 @@ const int ANALOG_PIN = A0;
 const int LED_PIN = 10;
 unsigned long accumulator = 0;
 unsigned long accumulator2 = 0;
+unsigned long accumulator3 = 0;
 const int interval = 250;
 float val = 0;
 float voltage = 0;
@@ -26,6 +27,8 @@ float temps[FILTER_COUNTS];
 int count = 0;
 float sum = 0;
 float avg = 0;
+float BLINK_DURATION = 10000;
+bool lightOn = 0;
 
 void setup() {
 	Serial.begin(9600);
@@ -35,7 +38,15 @@ void setup() {
 	//cannot add this line above, or will get -40 degrees
 }
 
-void loop() {
+// Math:
+// 1)
+// Vmeasured = analogRead()*1.1V/1023
+// Temp = Vmeasured*100-50
+// 2)
+// timePeriod = 60000/(7Tc-30) milliseconds per chip
+// BLINK_DURATION = (1/2) * timePeriod = 30000/(7Tc-30)
+
+void loop() {	
 	if(millis() - accumulator2 > interval/FILTER_COUNTS) { 
 		accumulator2 += interval/FILTER_COUNTS; 
 		val = analogRead(ANALOG_PIN);
@@ -64,5 +75,20 @@ void loop() {
 		sum = 0;
 		count = 0;
 		Serial.println(avg);
+		BLINK_DURATION = 30000/(7*avg-30);
 	}
+
+	// cricket - LED blinking
+	if (millis() - accumulator3 > BLINK_DURATION){
+		accumulator3 += BLINK_DURATION;
+		if (lightOn == 0){
+			digitalWrite(LED_PIN,HIGH);
+			lightOn = 1;
+		}
+		else{
+			digitalWrite(LED_PIN,LOW);
+			lightOn = 0;
+		}
+	}
+	
 }

a0e7b9acfb9e8d9d52b185444b9e39d0044d227c
Yangchen Shen <shyangch@gmail.com>
2016-09-28 12:11:40 -0500

    add 2nd timer (lab3

--- assignments/assign3/cricket/cricket.ino
+++ assignments/assign3/cricket/cricket.ino
@@ -13,14 +13,15 @@
  *  WUSTL Key:
  */
 
-const int ANALOG_PIN = A1;
+const int ANALOG_PIN = A0;
 const int LED_PIN = 10;
 unsigned long accumulator = 0;
+unsigned long accumulator2 = 0;
 const int interval = 250;
 float val = 0;
 float voltage = 0;
 float temperature = 0;
-const int FILTER_COUNTS = 5;
+const int FILTER_COUNTS = 10;
 float temps[FILTER_COUNTS]; 
 int count = 0;
 float sum = 0;
@@ -30,31 +31,38 @@ void setup() {
 	Serial.begin(9600);
 	analogReference(INTERNAL);
 	pinMode(LED_PIN, OUTPUT);
-	pinMode(ANALOG_PIN, OUTPUT);
-	//digitalWrite(LED_PIN,HIGH);
+	//pinMode(ANALOG_PIN, OUTPUT);
+	//cannot add this line above, or will get -40 degrees
 }
 
 void loop() {
+	if(millis() - accumulator2 > interval/FILTER_COUNTS) { 
+		accumulator2 += interval/FILTER_COUNTS; 
+		val = analogRead(ANALOG_PIN);
+		voltage = val*1.1/1023;
+		temperature = 100*voltage - 50;
+		temps[count % FILTER_COUNTS] = temperature;
+		count = count + 1;
+	}
+
 	if(millis() - accumulator > interval) { 
 		accumulator += interval; 
 		val = analogRead(ANALOG_PIN);
-		Serial.println(val);
+		//Serial.println(val);
 		voltage = val*1.1/1023;
 		temperature = 100*voltage - 50;
-		Serial.println(temperature);
 
-		temps[count % FILTER_COUNTS] = temperature;
-		count = count + 1;
+		//unfiltered data
+		Serial.print(temperature);
+		Serial.print(",");
 
-		if (count == FILTER_COUNTS){
-			for (int i = 0; i < FILTER_COUNTS; i++){
-				sum = sum + temps[i];
-			}
-			avg = sum/FILTER_COUNTS;
-			sum = 0;
-			count = 0;
-			Serial.print("avg: ");
-			Serial.println(avg);
+		//filtered data
+		for (int i = 0; i < FILTER_COUNTS; i++){
+			sum = sum + temps[i];
 		}
+		avg = sum/FILTER_COUNTS;
+		sum = 0;
+		count = 0;
+		Serial.println(avg);
 	}
 }

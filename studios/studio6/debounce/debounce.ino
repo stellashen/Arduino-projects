348def62f607ba1169439ea082c89072a851b159
Yangchen Shen <shyangch@gmail.com>
2016-10-10 16:56:36 -0500

    update studio6

---
+++ studios/studio6/debounce/debounce.ino
@@ -0,0 +1,65 @@
+// original code from : https://www.arduino.cc/en/Tutorial/Debounce
+// edited
+
+const int buttonPin = 3;
+const int ledPin = 8;
+
+int buttonState;             // the current reading from the input pin
+int lastButtonState = 1;   // the previous reading from the input pin
+int countPush = 0;
+
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
+void setup() {
+	Serial.begin(9600); 
+	pinMode(buttonPin, INPUT_PULLUP);// 0 when pushed, 1 when not
+	pinMode(buttonPin, INPUT);
+	pinMode(ledPin, OUTPUT);
+
+	// set initial LED state
+	digitalWrite(ledPin, LOW);
+}
+
+void loop() {
+	int reading = digitalRead(buttonPin);
+//	Serial.print("button reading: ");
+//	Serial.println(reading);
+
+	// check to see if you just pressed the button
+	// (i.e. the input went from 1 to 0),  and you've waited
+	// long enough since the last press to ignore any noise:
+
+	// If the switch changed, due to noise or pressing:
+	if (reading != lastButtonState) {
+		// reset the debouncing timer
+		lastDebounceTime = millis();
+	}
+
+	if ((millis() - lastDebounceTime) > debounceDelay) {
+		// whatever the reading is at, it's been there for longer
+		// than the debounce delay, so take it as the actual current state:
+
+		Serial.print("interval: ");
+		Serial.println(millis() - lastDebounceTime);
+		// it will be longer and longer if you don't push the button
+		
+		// if the button state has changed:
+		if (reading != buttonState) {
+			buttonState = reading;
+			countPush = countPush + 1;
+			Serial.println(countPush);
+
+			if (buttonState == 0) {
+				ledState = !ledState;
+			}
+		}
+	}
+
+	// set the LED:
+	digitalWrite(ledPin, ledState);
+
+	// save the reading.  Next time through the loop,
+	// it'll be the lastButtonState:
+	lastButtonState = reading;
+}
---
+++ studios/studio6/mirror/mirror.ino
@@ -0,0 +1,53 @@
+const int BUTTON_PIN = 3;
+const int LED_PIN = 8;
+int sensorValue1 = 1;
+int sensorValue2 = 1;
+int count = 0;
+int countPush = 0;
+
+void setup() {
+	// put your setup code here, to run once:
+	Serial.begin(9600);
+	pinMode(BUTTON_PIN, INPUT_PULLUP);
+	//digitalRead(BUTTON_PIN) would be 0 when pushed, 1 when not
+	pinMode(BUTTON_PIN, INPUT);
+	pinMode(LED_PIN, OUTPUT);
+	digitalWrite(LED_PIN,LOW); 
+}
+
+void loop() {
+	// put your main code here, to run repeatedly:
+	// use sensorValue1 to store the value for the 0th,2th,4th,...time the loop runs
+	if (count%2 == 0){
+		sensorValue1 = digitalRead(BUTTON_PIN);
+		//flash LED when the button is pushed
+		if (sensorValue1 == 0){
+			digitalWrite(LED_PIN,HIGH);
+		}
+		else{
+			digitalWrite(LED_PIN,LOW); 
+		}
+
+		if (sensorValue1 == 0 && sensorValue2 == 0){
+			countPush = countPush + 1;
+			Serial.println(countPush);
+		}
+	}
+	// use sensorValue2 to store the value for the 1th,3th,5th,...time the loop runs
+	else {
+		sensorValue2 = digitalRead(BUTTON_PIN);
+		if (sensorValue2 == 0){
+			digitalWrite(LED_PIN,HIGH);
+		}
+		else{
+			digitalWrite(LED_PIN,LOW); 
+		}
+
+		if (sensorValue1 == 1 && sensorValue2 == 0){
+			countPush = countPush + 1;
+			Serial.println(countPush);
+		}
+	}
+
+	count = count + 1;// record number of times the loop runs
+}

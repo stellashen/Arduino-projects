abc555280b280f12669a0bdd9f43043057340d0e
Yangchen Shen <shyangch@gmail.com>
2016-10-10 21:58:05 -0500

    update lab5

--- assignments/assign5/MorseCoder/MorseCoder.ino
+++ assignments/assign5/MorseCoder/MorseCoder.ino
@@ -1,13 +1,16 @@
 #include"MorseCodes.h"
 
 const int led = 13;
+int timeUnit = 2000;//slow testing
+//int timeUnit = 500;
+unsigned long accumulator = 0;
 
 // Argument: Any character
 // Return Value: Either:
 //                  1) If the character is a letter, the upper case equivalent.  
 //                  2) If the character is not a letter, the original value.
 char toUpper(char c) {
-  // TODO
+	// TODO
 	// c is a-z
 	if (c>=97 && c<=122){
 		return (char)(c-32);
@@ -19,34 +22,102 @@ char toUpper(char c) {
 }
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(led, OUTPUT);
+	Serial.begin(9600);
+	pinMode(led, OUTPUT);
 }
 
 
 void convertIncomingCharsToMorseCode() {
-  // TODO
-	// String words = incoming stream
+	// TODO
+	String words = "";
+	while(Serial.available() > 0){
+		char ch = Serial.read();
+		words = words + toUpper(ch);
+	}
+	// Serial.print(words);
 	for(int i=0;i<words.length();i++){
-	morseEncode(words[i]);
+		String letter = morseEncode(words[i]);
+		//***print for testing****
+		//		Serial.print("The morse code for ");
+		//		Serial.print(words[i]);
+		//		Serial.print(" is ");
+		//		Serial.println(letter);
+
+		Serial.print(letter);
+
+		// ' ' indicates: between words		
+		if(words[i]==' '){
+			if(millis() - accumulator > 4*timeUnit){
+				accumulator += 4*timeUnit; 
+				digitalWrite(led,LOW);
+				ledBlink(words[i]);
+			}
+		}
+		else {
+			ledBlink(words[i]);
+		}
+
 	}
 }
 
+// added this function to make led blink
+void ledBlink(String s){
+	for(int j=0; j<s.length();j++){
+		//***print for testing****
+		//			Serial.print("Code");
+		//			Serial.print(j);
+		//			Serial.print(" is ");
+		//			Serial.println(letter[i]);
 
+		// 1 unit between the codes within the same letter
+		if(millis() - accumulator > timeUnit){
+			accumulator += timeUnit; 
+			if(letter[i]=='.'){
+				digitalWrite(led, HIGH);
+				if(millis() - accumulator > timeUnit) { 
+					accumulator += timeUnit; 
+					digitalWrite(led, LOW);
+				}
+			}
+			if(letter[i]=='-'){
+				digitalWrite(led, HIGH);
+				if(millis() - accumulator > 3*timeUnit) { 
+					accumulator += 3*timeUnit; 
+					digitalWrite(led, LOW);
+				}
+			}
+			digitalWrite(led, LOW);
+		}
+		//two more units between the codes for two letters
+		if(millis() - accumulator > 2*timeUnit){
+			accumulator += 2*timeUnit; 
+			digitalWrite(led, LOW);
+		}
+	}
+}
 
+/*
+ * print testing output:
+The morse code for H is ....
+Code0 is .
+Code1 is .
+Code2 is .
+Code3 is .
+ * 
+ */
 
 void loop() {
-  // No Need to Modify this.  Put most of your code in "convertIncomingCharsToMorseCode()"
-  convertIncomingCharsToMorseCode();
-  
-  // test toUpper()
-//  char trya = 'a';
-//  char tryB = 'B';
-//  char try0 = '0';
-//  char tryOther = '!';
-//  Serial.println(toUpper(trya));
-//  Serial.println(toUpper(tryB));
-//  Serial.println(toUpper(try0));
-//  Serial.println(toUpper(tryOther));
-  
+	// No Need to Modify this.  Put most of your code in "convertIncomingCharsToMorseCode()"
+	convertIncomingCharsToMorseCode();
+
+	// test toUpper()
+	//  char trya = 'a';
+	//  char tryB = 'B';
+	//  char try0 = '0';
+	//  char tryOther = '!';
+	//  Serial.println(toUpper(trya));
+	//  Serial.println(toUpper(tryB));
+	//  Serial.println(toUpper(try0));
+	//  Serial.println(toUpper(tryOther));
+
 }
--- assignments/assign5/morse/ViewOutputStream.java
+++ assignments/assign5/morse/ViewOutputStream.java
@@ -1,10 +1,35 @@
 package assign5.morse;
 
-public class ViewOutputStream {
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import javax.swing.JFrame;
 
+import studio4.PrintStreamPanel;
+
+public class ViewOutputStream extends FilterOutputStream {
+
+	final private PrintStreamPanel psp;
+    final private PrintStream ps;
+    
+	public ViewOutputStream(OutputStream out) {
+		super(out);
+        JFrame f = new JFrame("ViewInputStream");
+		f.setBounds(100,100,225,300);
+        psp = new PrintStreamPanel();
+		f.getContentPane().add(psp);
+		f.setVisible(true);
+        ps = psp.getPrintStream(); 
+	}
+	
+	@Override
+	public void write(int i) throws IOException {
+	    // Send the Hex values corresponding to i (the parameter) 
+	    // to be displayed in the panel and also send it to the parent 
+	    // class so it can do its "write"
+		
 	}
 
 }

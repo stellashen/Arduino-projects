6fbb1ad8abf6291c044bc686bd2a1ca1d90b2a8d
Yangchen Shen <shyangch@gmail.com>
2016-10-25 22:46:25 -0500

    with testing code

--- assignments/assign6/display/display.ino
+++ assignments/assign6/display/display.ino
@@ -28,8 +28,8 @@ int buttonStateIn = 1;             // the current button state
 int buttonStateDe = 1; 
 int lastButtonStateIn = 1;   // the previous button state
 int lastButtonStateDe = 1;
-int countPushIn = 0;         // number of times the button is pushed
-int countPushDe = 0; 
+//int countPushIn = 0;         // number of times the button is pushed
+//int countPushDe = 0; 
 int readingValIn = 1010;     // input reading raw value: <20 when pushed, >1000 when not
 int readingValDe = 1010;
 int readingIn = 1;           // input reading: 0 when pushed, 1 when not
@@ -39,7 +39,10 @@ int lastReadingDe = 1;
 unsigned long lastDebounceTimeIn = 0;  // the last time the output pin was toggled
 unsigned long lastDebounceTimeDe = 0;
 unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
-bool b = true; // is buttonIn? true/false
+
+//unsigned long column0, column1, column2, column3, column4;
+int led[5][7];
+unsigned long columnVal = 0;
 
 void setup ()
 {
@@ -47,20 +50,20 @@ void setup ()
 	pinMode(buttonIn, INPUT_PULLUP);
 	pinMode(buttonDe, INPUT_PULLUP);
 
-	for(int i = 0; i < 7; i++){
-		pinMode(row[i], OUTPUT);
-	}
-	for(int i = 0; i < 5; i++){
-		pinMode(col[i], OUTPUT);
-	}
-
-	// all LEDs off
-	for(int i = 0; i < 7; i++){
-		digitalWrite(row[i],LOW);
-	}
-	for(int i = 0; i < 5; i++){
-		digitalWrite(col[i],HIGH);
-	}
+//	for(int i = 0; i < 7; i++){
+//		pinMode(row[i], OUTPUT);
+//	}
+//	for(int i = 0; i < 5; i++){
+//		pinMode(col[i], OUTPUT);
+//	}
+//
+//	// all LEDs off
+//	for(int i = 0; i < 7; i++){
+//		digitalWrite(row[i],LOW);
+//	}
+//	for(int i = 0; i < 5; i++){
+//		digitalWrite(col[i],HIGH);
+//	}
 }
 
 void loop ()
@@ -69,79 +72,97 @@ void loop ()
 	if (readingValIn < 20){
 		readingIn = 0;
 	}
-	else{
+	if(readingValIn > 1000){
 		readingIn = 1;
 	}
 
 	readingValDe = analogRead(buttonDe);
-	if (readingValIn < 20){
+	if (readingValDe < 20){
 		readingDe = 0;
 	}
-	else{
+	if(readingValDe > 1000){
 		readingDe = 1;
 	}
 
-	numChar = calculateNumChar();
+	//Serial.println(readingValIn);
+	//Serial.println(readingValDe);
 
-	Serial.println(numChar);
-	//font_5x7[c][0]
+	debounceIn();
+	debounceDe();
+
+	//Serial.println(numChar);
+
+	//corresponding values for each column
+	//	column0 = font_5x7[numChar][0];
+	//	column1 = font_5x7[numChar][1];
+	//	column2 = font_5x7[numChar][2];
+	//	column3 = font_5x7[numChar][3];
+	//	column4 = font_5x7[numChar][4];
+	//Serial.println(column0,BIN);
+
+	for (int i = 0; i < 5; i++){
+		for (int j = 0; j < 7; j++){
+			numChar = 0x03;
+			columnVal = font_5x7[numChar][i];
+			led[i][j] =  (columnVal >> (7-j) ) & 1; 
+			// returns 0 or 1
+			// the most important bit is set to row 0
+			// the least important bit is an extra useless bit
+			
+			Serial.print("Column ");
+			Serial.println(i);
+			Serial.print("Row ");
+			Serial.println(j);
+			Serial.println(led[i][j]);
+		}
+	}
+
 }
 
-
-int calculateNumChar() {
-	countPushIn = debounce(b);	
-	countPushDe = debounce(!b);
-	return (numChar+countPushIn-countPushDe);
-}
-
-int debounce(b){
-	if (b){
-		int buttonState = buttonStateIn;            
-		int lastButtonState = lastButtonStateIn;   
-		int countPush = countPushIn;
-		int reading = readingIn;           
-		int lastReading = lastReadingIn;
-		unsigned long lastDebounceTime = lastDebounceTimeIn;
-	}
-	else{
-		int buttonState = buttonStateDe;            
-		int lastButtonState = lastButtonStateDe;   
-		int countPush = countPushDe;
-		int reading = readingDe;           
-		int lastReading = lastReadingDe;
-		unsigned long lastDebounceTime = lastDebounceTimeDe;
-	}
+void debounceIn(){
 
 	// If the switch changed, due to noise or pressing:
-	if (reading != lastReading) {
+	if (readingIn != lastReadingIn) {
 		// reset the debouncing timer
-		lastDebounceTime = millis();
+		lastDebounceTimeIn = millis();
 	}
 
 	// if wait long enough, take the reading as the real button state
-	if (millis() - lastDebounceTime > debounceDelay) {
-		buttonState = reading;
+	if (millis() - lastDebounceTimeIn > debounceDelay) {
+		buttonStateIn = readingIn;
 
-		if (buttonState==0 && lastButtonState==1){
-			countPush = countPush + 1;
-			Serial.println(countPush);
+		if (buttonStateIn==0 && lastButtonStateIn==1){
+			//countPushIn = countPushIn + 1;
+			//Serial.println(countPushIn);
+			numChar = numChar + 1;
 		}
 
-		lastButtonState = buttonState;
+		lastButtonStateIn = buttonStateIn;
 	}
 
-	if(b){
-		buttonStateIn = buttonState;            
-		lastButtonStateIn = lastButtonState;    
-		lastDebounceTimeIn = lastDebounceTime;
-		lastReadingIn = reading;
-	}
-	else{
-		buttonStateDe = buttonState;            
-		lastButtonStateDe = lastButtonState;    
-		lastDebounceTimeDe = lastDebounceTime;
-		lastReadingDe = reading;
+	lastReadingIn = readingIn;
+}
+
+void debounceDe(){
+
+	// If the switch changed, due to noise or pressing:
+	if (readingDe != lastReadingDe) {
+		// reset the debouncing timer
+		lastDebounceTimeDe = millis();
 	}
 
-	return countPush;
+	// if wait long enough, take the reading as the real button state
+	if (millis() - lastDebounceTimeDe > debounceDelay) {
+		buttonStateDe = readingDe;
+
+		if (buttonStateDe==0 && lastButtonStateDe==1){
+			//countPushDe = countPushDe + 1;
+			//Serial.println(countPushDe);
+			numChar = numChar - 1;
+		}
+
+		lastButtonStateDe = buttonStateDe;
+	}
+
+	lastReadingDe = readingDe;
 }

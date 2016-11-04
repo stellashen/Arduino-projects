9809546253fab8857f8402453748f85cd8288df5
Yangchen Shen <shyangch@gmail.com>
2016-11-03 22:53:38 -0500

    lab9

--- assignments/assign9/FitbitPlot.java
+++ assignments/assign9/FitbitPlot.java
@@ -14,16 +14,23 @@ import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import sedgewick.StdDraw;
+import java.awt.Font;
+
 import studio4.SerialComm;
 
 public class FitbitPlot {
 	final private DataInputStream d;
 	private int header,key;
+	private int count = 0;
+	private float[] readX = new float[45];
+	private float[] readY = new float[45];
+	private float[] readZ = new float[45];
 
 	public FitbitPlot(InputStream in){
 		d = new DataInputStream(in);
 	}
-	
+
 	//FSM: each state represents different data types; 
 	//an idle state for waiting for the magic number
 
@@ -71,6 +78,63 @@ public class FitbitPlot {
 			}
 			break;
 
+		case x:
+			// when point45 show up, replace the last point (point44 on the far right) with the newest point
+			// count will always stay 44, so the new data will always be plotted on the right location
+			if (count > 44){
+				count = 44;
+				//ignore the original point0,
+				//and move the original point1-44 to the left so they become the new point0-43
+				for (int i = 0; i < 44; i++){
+					readX[i]=readX[i+1];
+				}
+				readX[count] = d.readFloat();
+				System.out.print("x:");
+				System.out.println(readX[count]);
+				//set count to 45 again so it can be used by case y
+				count = 45;
+			}
+			else{
+				readX[count] = d.readFloat();
+				System.out.print("x:");
+				System.out.println(readX[count]);
+			}
+			state = State.idle;
+			break;
+
+		case y:
+			if (count > 44){
+				count = 44;
+				for (int i = 0; i < 44; i++){
+					readY[i]=readY[i+1];
+				}
+				readY[count] = d.readFloat();
+				System.out.print("y:");
+				System.out.println(readY[count]);
+				//set count to 45 again so it can be used by case z
+				count = 45;
+			}
+			else{
+				readY[count] = d.readFloat();
+				System.out.print("y:");
+				System.out.println(readY[count]);
+			}
+			state = State.idle;
+			break;
+
+		case z:
+			if (count > 44){
+				count = 44;
+				for (int i = 0; i < 44; i++){
+					readZ[i]=readZ[i+1];
+				}
+			}
+			readZ[count] = d.readFloat();
+			System.out.print("z:");
+			System.out.println(readZ[count]);
+			state = State.idle;
+			break;
+
 		case up0:
 			System.out.println();
 			System.out.print("debugging string:");
@@ -111,24 +175,7 @@ public class FitbitPlot {
 			System.out.print("timestamp:");
 			int t = d.readInt();
 			System.out.println(t);
-			state = State.idle;
-			break;
-		case x:
-			System.out.print("x:");
-			float readX = d.readFloat();
-			System.out.println(readX);
-			state = State.idle;
-			break;
-		case y:
-			System.out.print("y:");
-			float readY = d.readFloat();
-			System.out.println(readY);
-			state = State.idle;
-			break;
-		case z:
-			System.out.print("z:");
-			float readZ = d.readFloat();
-			System.out.println(readZ);
+			draw();
 			state = State.idle;
 			break;
 		}
@@ -143,19 +190,48 @@ public class FitbitPlot {
 		{
 			while(true){
 				if (d.available()>0){
-			counterState = nextState(counterState);
+					counterState = nextState(counterState);
 				}
 			}
 		}
-
 		catch ( Exception e )
 		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}	
 	}
-	
+
+	//refresh the image every 1 second
+	public void draw(){
+		//clear the canvas
+		StdDraw.clear();
+		count = count + 1;
+		
+		for (int i = 0; i < count; i++){
+			plot(readX[i],readY[i],readZ[i],i);
+		}
+
+		StdDraw.show();
+	}
+
+	public void plot(float x,float y,float z,int i){
+		StdDraw.setPenRadius(0.01);
+		StdDraw.setPenColor(StdDraw.BLUE);
+		StdDraw.point(5+i,5+x);
+		StdDraw.setPenColor(StdDraw.ORANGE);
+		StdDraw.point(5+i,5+y);
+		StdDraw.setPenColor(StdDraw.RED);
+		StdDraw.point(5+i,5+z);
+	}
+
+
 	public static void main(String[] args) {
+		StdDraw.enableDoubleBuffering();
+		//set size
+		StdDraw.setCanvasSize(800, 600);
+		StdDraw.setXscale(0, 60);
+		StdDraw.setYscale(0, 10);
+
 		try
 		{        	
 			SerialComm s = new SerialComm();
@@ -169,6 +245,7 @@ public class FitbitPlot {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
 
 	}
 

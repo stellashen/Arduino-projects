2a7191c4fad7d09d8bbb9b77541708249780b160
Yangchen Shen <shyangch@gmail.com>
2016-12-07 13:09:06 -0600

    finish lab13

--- assignments/assign13/assignment13Demo/assignment13Demo.ino
+++ assignments/assign13/assignment13Demo/assignment13Demo.ino
@@ -16,27 +16,27 @@ void setup() {
   Serial.print("Should be 444: ");
   Serial.println(sumArray(c, 10));
 
-  Serial.print("Should be 175: ");
+  Serial.print("Should be 116: ");
   Serial.println(avgArray(b, 3));
 
   Serial.print("Should be 44: ");
-  Serial.println(avgArray(c, 3));
+  Serial.println(avgArray(c, 10));
 
   Serial.print("Should be 3: ");
   Serial.println(divide(21, 7));
 
   Serial.print("Should be 9: ");
   Serial.println(divide(95, 10));
-//
-//  byte d[] = {1, 2, 3};
-//  byte e[] = {4, 5, 6};
-//  Serial.print("Should be 32: ");
-//  Serial.println(dot(d, e, 3));
-//
-//  byte f[] = {7, 20, 19, 11};
-//  byte g[] = {132, 51, 0, 84, 30};
-//  Serial.print("Should be 2988: ");
-//  Serial.println(dot(f, g, 5));
+
+  byte d[] = {1, 2, 3};
+  byte e[] = {4, 5, 6};
+  Serial.print("Should be 32: ");
+  Serial.println(dot(d, e, 3));
+
+  byte f[] = {7, 20, 19, 11, 4};
+  byte g[] = {132, 51, 0, 84, 30};
+  Serial.print("Should be 2988: ");
+  Serial.println(dot(f, g, 5));
 }
 
 void loop() { /* unused */ }
